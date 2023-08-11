ed90eff76e5c86a863014f984fb56b865be52b23
guillaumecamanes <guillaume.camanes@mobiapps.fr>
2023-08-11 15:18:26 +0200

    added theme + color scheme

---
+++ lib/core/theme/colors.helper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class ColorsHelper {
+  const ColorsHelper._();
+
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xFF000000);
+  static const Color grey = Color(0xFFB2B2B2);
+  static const Color primary = Color(0xFF0C549D);
+  static const Color secondary = Color(0xFF013758);
+  static const Color tertiary = Color(0xFF50D2FF);
+  static const Color background = Color(0xFF121212);
+  static const Color surface = Color(0xFF1E1E1E);
+  static const Color error = Color(0xFFBA1A1A);
+  static const Color errorDark = Color(0xFFCF6679);
+}
---
+++ lib/core/theme/theme.helper.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mangatek/core/theme/colors.helper.dart';
+
+@immutable
+class ThemeHelper {
+  const ThemeHelper._();
+
+  static ThemeData theme = ThemeData(
+    useMaterial3: true,
+    colorScheme: _colorScheme,
+  );
+
+  static final ColorScheme _colorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.light,
+    seedColor: ColorsHelper.primary,
+    secondary: ColorsHelper.secondary,
+    secondaryContainer: ColorsHelper.secondary,
+    background: ColorsHelper.background,
+    surface: ColorsHelper.surface,
+    error: ColorsHelper.error,
+    onPrimary: ColorsHelper.white,
+    onSecondary: ColorsHelper.white,
+    onBackground: ColorsHelper.white,
+    onSurface: ColorsHelper.white,
+    onError: ColorsHelper.white,
+  );
+}
